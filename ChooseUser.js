ee6fba387c72835316c7193276cc73cb43f31cfe
RITHIKA P <140421900+Rithikap-8@users.noreply.github.com>
2024-02-21 16:52:20 +0530

    Add files via upload

---
+++ ChooseUser.js
@@ -0,0 +1,148 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  Grid,
+  Paper,
+  Box,
+  Container,
+  CircularProgress,
+  Backdrop,
+} from '@mui/material';
+import { AccountCircle, School, Group } from '@mui/icons-material';
+import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUser } from '../redux/userRelated/userHandle';
+import Popup from '../components/Popup';
+
+const ChooseUser = ({ visitor }) => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const password = "zxc"
+
+  const { status, currentUser, currentRole } = useSelector(state => state.user);;
+
+  const [loader, setLoader] = useState(false)
+  const [showPopup, setShowPopup] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const navigateHandler = (user) => {
+    if (user === "Admin") {
+     
+        navigate('/Adminlogin');
+      
+    }
+
+    else if (user === "Student") {
+      
+        navigate('/Studentlogin');
+      
+    }
+
+    else if (user === "Teacher") {
+     
+        navigate('/Teacherlogin');
+      
+    }
+  }
+
+  useEffect(() => {
+    if (status === 'success' || currentUser !== null) {
+      if (currentRole === 'Admin') {
+        navigate('/Admin/dashboard');
+      }
+      else if (currentRole === 'Student') {
+        navigate('/Student/dashboard');
+      } else if (currentRole === 'Teacher') {
+        navigate('/Teacher/dashboard');
+      }
+    }
+    else if (status === 'error') {
+      setLoader(false)
+      setMessage("Network Error")
+      setShowPopup(true)
+    }
+  }, [status, currentRole, navigate, currentUser]);
+
+  return (
+    <StyledContainer>
+      <Container>
+        <Grid container spacing={2} justifyContent="center" sx={{ my: 15 }}>
+          <Grid item xs={12} sm={6} md={4} >
+            <div onClick={() => navigateHandler("Admin")}>
+              <StyledPaper elevation={3} >
+                <Box mb={2}>
+                  <AccountCircle fontSize="large" />
+                </Box>
+                <StyledTypography>
+                  Admin
+                </StyledTypography>
+               
+              </StyledPaper>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <StyledPaper elevation={3}>
+              <div onClick={() => navigateHandler("Student")}>
+                <Box mb={2}>
+                  <School fontSize="large" />
+                </Box>
+                <StyledTypography>
+                  Student
+                </StyledTypography>
+               
+              </div>
+            </StyledPaper>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <StyledPaper elevation={3}>
+              <div onClick={() => navigateHandler("Teacher")}>
+                <Box mb={2}>
+                  <Group fontSize="large" />
+                </Box>
+                <StyledTypography>
+                  Teacher
+                </StyledTypography>
+               
+              </div>
+            </StyledPaper>
+          </Grid>
+        </Grid>
+      </Container>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loader}
+      >
+        <CircularProgress color="inherit" />
+        Please Wait
+      </Backdrop>
+      <Popup message={message} setShowPopup={setShowPopup} showPopup={showPopup} />
+    </StyledContainer>
+  );
+};
+
+export default ChooseUser;
+
+const StyledContainer = styled.div`
+  background: linear-gradient(to bottom, #411d70, #19118b);
+  height: 120vh;
+  display: flex;
+  justify-content: center;
+  padding: 2rem;
+`;
+
+const StyledPaper = styled(Paper)`
+  padding: 20px;
+  text-align: center;
+  background-color: #1f1f38;
+  color:rgba(255, 255, 255, 0.6);
+  cursor:pointer;
+
+  &:hover {
+    background-color: #2c2c6c;
+    color:white;
+  }
+`;
+
+const StyledTypography = styled.h2`
+  margin-bottom: 10px;
+`;
